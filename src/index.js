3c5294f338833d2d8b95bd85e328f2d6b1490ea4
Mohammed Ameen <jegroamin@gmail.com>
2018-09-01 15:59:41 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Address from './components/Address';
 ReactDOM.render(
   <div>Components Need To Be Mounted</div>,
   document.getElementById('root')
