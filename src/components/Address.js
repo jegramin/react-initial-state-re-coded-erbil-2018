a88131b53fc8ab1c1609cde7221aa3ed916f1738
Mohammed Ameen <jegroamin@gmail.com>
2018-09-01 16:00:36 +0000

    automatically backed up by learn

--- src/components/Address.js
+++ src/components/Address.js
@@ -11,9 +11,9 @@ class Address extends React.Component {
  
   render() {
     return (
-      <div className="address">
-        {this.state.fullAddress}
-      </div>
+      
+        <div className="address">{this.props.street}, {this.props.city}</div>
+      
     );
   }
 }
