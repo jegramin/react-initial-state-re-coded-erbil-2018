4310f6b3c64b3b534d1631ff8735ea4f6915534e
Mohammed Ameen <jegroamin@gmail.com>
2018-09-01 16:02:54 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Address from './components/Address';
+import ToggleButton from './components/ToggleButton';
 
 ReactDOM.render(
   <div>
